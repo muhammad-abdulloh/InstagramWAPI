589490c61267519eb5f5a13213de0c593ba13478
Clever Coder <clevercoderr@gmail.com>
2022-03-28 13:22:03 +0500

    Deploy Heroku Database in Postgresql

---
+++ Instagram.Data/Migrations/20220328080715_Thirth2Migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Instagram.Data.Migrations
+{
+    public partial class Thirth2Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
